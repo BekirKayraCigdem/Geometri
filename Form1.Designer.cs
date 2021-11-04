8b13a2ad3ce1400a041ef5536a7cc0a701b31b71
Bekir Kayra Çiğdem <93601074+BekirKayraCigdem@users.noreply.github.com>
2021-11-04 13:54:20 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,421 @@
+﻿namespace geometry
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.alanLabel = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.cevreLabel = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(737, 272);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 61;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(656, 272);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 60;
+            this.button4.Text = "Calculate";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // alanLabel
+            // 
+            this.alanLabel.AutoSize = true;
+            this.alanLabel.Location = new System.Drawing.Point(734, 233);
+            this.alanLabel.Name = "alanLabel";
+            this.alanLabel.Size = new System.Drawing.Size(16, 17);
+            this.alanLabel.TabIndex = 59;
+            this.alanLabel.Text = "0";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(681, 207);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(110, 17);
+            this.label14.TabIndex = 58;
+            this.label14.Text = "Area of ​​Triangle";
+            // 
+            // cevreLabel
+            // 
+            this.cevreLabel.AutoSize = true;
+            this.cevreLabel.Location = new System.Drawing.Point(734, 181);
+            this.cevreLabel.Name = "cevreLabel";
+            this.cevreLabel.Size = new System.Drawing.Size(16, 17);
+            this.cevreLabel.TabIndex = 57;
+            this.cevreLabel.Text = "0";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(681, 164);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(171, 17);
+            this.label13.TabIndex = 56;
+            this.label13.Text = "Circumference of Triangle";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(711, 67);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 55;
+            this.textBox7.Text = "0";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(712, 95);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 54;
+            this.textBox8.Text = "0";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(712, 124);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 53;
+            this.textBox9.Text = "0";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(52, 176);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 46;
+            this.textBox4.Text = "0";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(52, 204);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 45;
+            this.textBox5.Text = "0";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(52, 232);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 44;
+            this.textBox6.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(52, 123);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 43;
+            this.textBox3.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(52, 95);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 42;
+            this.textBox2.Text = "0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 41;
+            this.textBox1.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(709, 45);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(91, 17);
+            this.label9.TabIndex = 52;
+            this.label9.Text = "Side Lengths";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(681, 124);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(23, 17);
+            this.label10.TabIndex = 51;
+            this.label10.Text = "c :";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(681, 94);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(24, 17);
+            this.label11.TabIndex = 50;
+            this.label11.Text = "b :";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(682, 67);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(24, 17);
+            this.label12.TabIndex = 49;
+            this.label12.Text = "a :";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(424, 45);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(252, 221);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 48;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(106, 272);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 47;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 272);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 233);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 17);
+            this.label6.TabIndex = 40;
+            this.label6.Text = "z¹ :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 17);
+            this.label7.TabIndex = 39;
+            this.label7.Text = "y¹ :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(22, 176);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 17);
+            this.label8.TabIndex = 38;
+            this.label8.Text = "x¹ :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 17);
+            this.label5.TabIndex = 37;
+            this.label5.Text = "Exterior Angles";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 17);
+            this.label4.TabIndex = 36;
+            this.label4.Text = "Interior Angles";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 17);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "z :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 17);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "y :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 17);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "x :";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(166, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(252, 221);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 31;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 340);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.alanLabel);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.cevreLabel);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Geometry";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label alanLabel;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label cevreLabel;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace geometry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Double x =0, y=0, z=0, x1=0, y1=0, z1=0 ;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox7.Text = "0";
+            textBox8.Text = "0";
+            textBox9.Text = "0";
+            alanLabel.Text = "0";
+            cevreLabel.Text = "0";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox8.Text=="" || textBox7.Text=="" || textBox9.Text=="")
+            {
+                MessageBox.Show("Do not leave it blank.", "Error");
+            }
+
+            x = Convert.ToDouble(textBox7.Text);
+            y = Convert.ToDouble(textBox8.Text);
+            z = Convert.ToDouble(textBox9.Text);
+
+            double cevreUcgen = x + y + z;
+            cevreLabel.Text = cevreUcgen.ToString();
+
+            double u = cevreUcgen / 2;
+            double alanUcgen = Math.Sqrt(u * (u - x) * (u - y) * (u - z));
+            alanLabel.Text = alanUcgen.ToString();
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+            textBox4.Text = "0";
+            textBox5.Text = "0";
+            textBox6.Text = "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text=="")
+            {
+                textBox1.Text = "0";
+            }
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = "0";
+            }
+            if (textBox3.Text == "")
+            {
+                textBox3.Text = "0";
+            }
+            if (textBox4.Text == "")
+            {
+                textBox4.Text = "0";
+            }
+            if (textBox5.Text == "")
+            {
+                textBox5.Text = "0";
+            }
+            if (textBox6.Text == "")
+            {
+                textBox6.Text = "0";
+            }
+
+            x = Convert.ToDouble(textBox1.Text);
+            y = Convert.ToDouble(textBox2.Text);
+            z = Convert.ToDouble(textBox3.Text);
+            x1 = Convert.ToDouble(textBox4.Text);
+            y1 = Convert.ToDouble(textBox5.Text);
+            z1 = Convert.ToDouble(textBox6.Text);
+
+            if (x==0 && y==0 && z==0)
+            {
+                if (x1==0 && y1==0 && z1==0)
+                {
+                    MessageBox.Show("Do not leave it blank.", "Error");
+                }
+                else if (x1==0)
+                {
+                    x1 = 360 - (y1 + z1);
+                    x = 180 - x1;
+                    y = 180 - y1;
+                    z = 180 - z1;
+                }
+                else if (y1==0)
+                {
+                    y1 = 360 - (x1 + z1);
+                    x = 180 - x1;
+                    y = 180 - y1;
+                    z = 180 - z1;
+                }
+                else if (z1==0)
+                {
+                    z1 = 360 - (y1 + x1);
+                    x = 180 - x1;
+                    y = 180 - y1;
+                    z = 180 - z1;
+                }
+                else
+                {
+                    MessageBox.Show("Do not leave it blank.", "Error");
+                }
+
+            }
+
+            else if (x==0 && y==0)
+            {
+                z1 = 180 - z;
+
+                if (x1==0)
+                {
+                    x1 = 360 - (y1 + z1);
+                    x = 180 - x1;
+                    y = 180 - (x + z);
+                }
+
+                else if (y1==0)
+                {
+                    y1 = 360 - (x1 + y1);
+                    y = 180 - y1;
+                    x = 180 - (y + z);
+                }
+                
+
+            }
+            else if (x == 0 && z == 0)
+            {
+                y1 = 180 - y;
+
+                if (x1 == 0)
+                {
+                    x1 = 360 - (y1 + z1);
+                    x = 180 - x1;
+                    z = 180 - (x + y);
+                }
+
+                else if (z1 == 0)
+                {
+                    z1 = 360 - (x1 + y1);
+                    z = 180 - z1;
+                    x = 180 - (y + z);
+                }
+
+
+            }
+            else if (z == 0 && y == 0)
+            {
+                x1 = 180 - x;
+
+                if (z1 == 0)
+                {
+                    z1 = 360 - (y1 + x1);
+                    z = 180 - z1;
+                    y = 180 - (z + x);
+                }
+
+                else if (y1 == 0)
+                {
+                    y1 = 360 - (x1 + z1);
+                    y = 180 - y1;
+                    z = 180 - (x + y);
+                }
+
+
+            }
+
+            else if (x==0)
+            {
+                x = 180 - (y + z);
+                x1 = 180 - x;
+                y1 = 180 - y;
+                z1 = 180 - z;
+            }
+            else if (y == 0)
+            {
+                y = 180 - (x + z);
+                x1 = 180 - x;
+                y1 = 180 - y;
+                z1 = 180 - z;
+            }
+            else if (z == 0)
+            {
+                z = 180 - (y + x);
+                x1 = 180 - x;
+                y1 = 180 - y;
+                z1 = 180 - z;
+            }
+
+
+            if (x >=179 || y >= 179 || z >= 179 || x1 >= 179 || y1 >= 179 || z1 >= 179)
+            {
+                MessageBox.Show("It cannot be greater than 180.", "Error");
+            }
+            else
+            {
+                textBox1.Text = x.ToString();
+                textBox2.Text = y.ToString();
+                textBox3.Text = z.ToString();
+                textBox4.Text = x1.ToString();
+                textBox5.Text = y1.ToString();
+                textBox6.Text = z1.ToString();
+            }
+            
+        }
+
+
+    }
+}
